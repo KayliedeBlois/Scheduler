d5cb439797d2ed44c2afad0455815b6ca368ec98
KayliedeBlois <kayliedeblois@gmail.com>
2022-08-14 13:35:19 -0400

    Add props and state to Appointment Form and display Appointments in app

--- src/components/Application.js
+++ src/components/Application.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
 import InterviewerList from "./InterviewerList";
+import Appointment from "./Appointment";
 
 
 const days = [
@@ -30,11 +31,49 @@ const interviewers = [
   { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
 ];
 
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
   const [interviewer, setInterviewer] = useState("name");
-  const [student, setStudent] = useState("student name");
+  // const [student, setStudent] = useState("student name");
   
   return (
     <main className="layout">
@@ -56,10 +95,6 @@ export default function Application(props) {
           value={interviewer} 
           onChange={setInterviewer} 
         />
-        <Form 
-          value={student}
-          onChange={setStudent}
-        />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
@@ -68,7 +103,16 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {Object.values(appointments).map((appointment) => 
+          <Appointment 
+            key={appointment.id} 
+            {...appointment} 
+          />
+        )}
+        <Appointment 
+          key="last"
+          time="5pm"
+        /> 
       </section>
     </main>
   );
--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "components/Appointment/styles.scss"
 import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
 
 export default function Appointment(props) {
 
   return (
-    <article className="appointment"></article>
+    <>
+      <Header time={props.time}/>
+      <article className="appointment">
+        {props.interview ? <Show student={props.student} interviewer={props.interviewer} /> : <Empty/>}
+      </article>
+    </>
   );
 }

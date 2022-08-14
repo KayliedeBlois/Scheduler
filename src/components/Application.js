c17d7833aeeb64d8811062cf967c517e25812356
KayliedeBlois <kayliedeblois@gmail.com>
2022-08-13 23:01:25 -0400

    Create Interviewer List stories

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "./DayList";
+import InterviewerList from "./InterviewerList";
+
 
 const days = [
   {
@@ -20,9 +22,19 @@ const days = [
   },
 ];
 
+const interviewers = [
+  { id: 1, name: "Sylvia Palmer", avatar: "https://i.imgur.com/LpaY82x.png" },
+  { id: 2, name: "Tori Malcolm", avatar: "https://i.imgur.com/Nmx0Qxo.png" },
+  { id: 3, name: "Mildred Nazir", avatar: "https://i.imgur.com/T2WwVfS.png" },
+  { id: 4, name: "Cohana Roy", avatar: "https://i.imgur.com/FK8V841.jpg" },
+  { id: 5, name: "Sven Jones", avatar: "https://i.imgur.com/twYrpay.jpg" }
+];
+
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
-  const [interviewer, setInterviewer] = useState("Interviewer");
+  const [interviewer, setInterviewer] = useState("name");
+  
   return (
     <main className="layout">
       <section className="sidebar">
@@ -34,7 +46,7 @@ export default function Application(props) {
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
         <DayList days={days} day={day} setDay={setDay} />
-        
+        <InterviewerList interviewers={interviewers} interviewer={interviewer} setInterviewer={setInterviewer} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
--- src/components/InterviewerListItem.js
+++ src/components/InterviewerListItem.js
@@ -3,22 +3,22 @@ import "components/InterviewerListItem.scss";
 import classNames from "classnames";
 
 export default function InterviewerListItem (props) {
-let interviewerClass = classNames("interviewers__item", {
-  "interviewers__item--selected":props.selected,
-})
-
+  let interviewerClass = classNames("interviewers__item", {
+    "interviewers__item--selected":props.selected,
+  });
+console.log(props);
   const interviewerName = (props) => {
     if (props.selected) {
       return `${props.name}`;
     } else {
       return "";
     }
-  }
+  };
 
   return (
     <li className={interviewerClass} onClick={() => props.setInterviewer(props.id)}>
       <img
-        className= "interviewers__item-image"
+        className= {"interviewers__item-image"}
         src={props.avatar}
         alt={props.name}
       />

6d7d2da3b0a1f5153f0de8bf754b69b0c8e364b1
KayliedeBlois <kayliedeblois@gmail.com>
2022-08-16 17:04:24 -0400

    Create custom useVisualMode and test

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,9 +1,27 @@
 import { useState } from 'react';
 
 export default function useVisualMode(initial) {
-
   const [mode, setMode] = useState(initial);
+  const [history, setHistory] = useState([initial]);
 
-  return { mode };
+  function transition(newMode, replace = false) {
+    setHistory([...history, newMode]);
+    setMode(newMode);
+  }
+
+  function back() {
+    if (history.length > 1) {
+      setMode(history[history.length-2]);
+
+      console.log(history[history.length-2])
+
+      const newHistory = [...history];
+
+      setHistory(newHistory.slice(0, -1));
+
+    }
+  }
+
+  return { mode, transition, back };
 }
 

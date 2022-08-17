ad619206ffc182b3654689a75ba10089b8d56e8a
KayliedeBlois <kayliedeblois@gmail.com>
2022-08-16 23:06:00 -0400

    Add getInterviewersForDay function and test

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -30,3 +30,29 @@ export function getInterview(state, interview) {
 
   return {student: interview.student, interviewer: state.interviewers[interview.interviewer]}
 };
+
+
+export function getInterviewersForDay(state, name) {
+  
+  const filteredInterviewers = [];
+
+  if (state.days.length === 0) {
+    return [];
+  }
+
+  const filteredDay = state.days.filter(day => day.name === name);
+
+  if (filteredDay.length === 0) {
+    return [];
+  }
+
+  const appointments = filteredDay[0].appointments;
+
+  for(let appointment in state.appointments) {
+    if(appointments.includes(parseInt(appointment)) && state.appointments[appointment].interview) {
+      filteredInterviewers.push(state.interviewers[state.appointments[appointment].interview.interviewer]);
+    }
+  }
+
+  return filteredInterviewers;
+}
